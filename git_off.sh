1ad1c85f407481a619dd974096fa590df3bd3ebb
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-07-06 23:23:31 -0400

    Update git_off.sh to function

--- git_off.sh
+++ git_off.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
+if [[ $1 == "add" && $2 == "-d" ]]
+then
+  echo $3 >> empty.txt
 
+elif [[ $1 == "status" && $2 == "" && $3 == "" ]]
+then
+  empty=$(<empty.txt)
+  echo $empty
 
-line="* * * * * /usr/bin/google-chrome"
-# (crontab -u userhere -l; echo "$line" ) | crontab -u userhere -
-(crontab -l; echo "$line" ) | crontab -
-echo "$line"
+# elif [[ $1 == "-rm" && 2 == "" && $3 == "" ]]
+# then
+
+else
+  echo "Wrong syntax, idiot."
+fi
+# http://stackoverflow.com/questions/4749330/how-to-test-if-string-exists-in-file-with-bash-shell
