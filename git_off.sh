ffa3f42df78a2f5e4f328d2bed42a9596dfd2ac8
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-07-13 17:35:44 -0400

    almost done

--- git_off.sh
+++ git_off.sh
@@ -3,21 +3,21 @@
 if [[ $1 == "add" && $2 == "-d" ]]
 then
 #add directory
-  echo $3 >> empty.txt
+  echo $3 >> empty.csv
 # elif [[ $1 == "add" && $2 == "-f" ]]
 # then
 #   #add file
 elif [[ $1 == "status" && $2 == "" && $3 == "" ]]
 then
-  empty=$(<empty.txt)
+  empty=$(<empty.csv)
   echo $empty
 
 elif [[ $1 == "-rm" && $2 && $3 == "" ]]
   then
-  if grep -Fxq $2 empty.txt
+  if grep -Fxq $2 empty.csv
     then
-    echo `sed  /$2/d empty.txt` > empty.txt
-     perl -p -i -e 's/\s/\n/g' empty.txt
+    echo `sed  /$2/d empty.csv` > empty.csv
+     perl -p -i -e 's/\s/\n/g' empty.csv
   else
     echo "File not found"
   fi
@@ -25,3 +25,9 @@ else
   echo "Wrong syntax, idiot."
 
 fi
+
+
+# realpath git_off.sh
+
+var=$(pwd)
+echo $var
