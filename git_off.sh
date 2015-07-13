5d724c1f7404a8ea101e611cc590420527ff6425
Joseph Tessel <Tessel.Joseph@gmail.com>
2015-07-13 19:09:55 -0400

    add ability to add directory/file in csv file and format

--- git_off.sh
+++ git_off.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
 
-if [[ $1 == "add" && $2 == "-d" ]]
+if [[ $1 == "add" && $2 == "-A" ]]
 then
 #add directory
-  echo $3 >> empty.csv
+var=$(pwd)
+  echo "directory,$var,,\"$3\"" >> queue.csv
 # elif [[ $1 == "add" && $2 == "-f" ]]
 # then
 #   #add file
+elif [[ $1 == "add" ]]
+then
+#add directory
+var=$(pwd)
+  echo "file,$var,$2,\"$3\"" >> queue.csv
 elif [[ $1 == "status" && $2 == "" && $3 == "" ]]
 then
-  empty=$(<empty.csv)
-  echo $empty
+  queue=$(<queue.csv)
+  echo $queue
 
 elif [[ $1 == "-rm" && $2 && $3 == "" ]]
   then
-  if grep -Fxq $2 empty.csv
+  if grep -Fxq $2 queue.csv
     then
-    echo `sed  /$2/d empty.csv` > empty.csv
-     perl -p -i -e 's/\s/\n/g' empty.csv
+    echo `sed  /$2/d queue.csv` > queue.csv
+     perl -p -i -e 's/\s/\n/g' queue.csv
   else
     echo "File not found"
   fi
@@ -26,8 +32,4 @@ else
 
 fi
 
-
 # realpath git_off.sh
-
-var=$(pwd)
-echo $var
