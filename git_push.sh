e34632cec7177dde5195544554104c7ef6e3916e
Ethan Hathaway <erhathaway@gmail.com>
2015-07-12 22:28:51 -0400

    "this is a test of adding a folder"

--- git_push.sh
+++ git_push.sh
@@ -13,22 +13,15 @@ DIRECTORY="${Array[1]}"
 NAME="${Array[2]}" 
 COMMENT="${Array[3]}" 
 
-echo $ITEMTYPE
-echo $DIRECTORY
-echo $NAME
-echo $COMMENT
+# echo $ITEMTYPE
+# echo $DIRECTORY
+# echo $NAME
+# echo $COMMENT
 
 if [ "$ITEMTYPE" = "directory" ]; then
-  cd $DIRECTORY && git add -A && git commit -m "$COMMENT"
+  cd $DIRECTORY && git add -A && git commit -m $COMMENT
 elif [ "$ITEMTYPE" = "file" ]; then
-  cd $DIRECTORY && git add "$NAME" && git commit -m "$COMMENT"
+  cd $DIRECTORY && git add $NAME && git commit -m $COMMENT
 fi
 
-
-# get file or directory off queue
-# cd into directory
-# if directory, git add -add
-# elif file, git add file
-
-# git commit -m message
-# git push 
+cd 
