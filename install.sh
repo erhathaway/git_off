41709b62dfbf3a179359a630fc3a39b21bcf3d7c
Ethan Hathaway <erhathaway@gmail.com>
2015-08-01 19:06:02 -0400

    update cron

--- install.sh
+++ install.sh
@@ -40,7 +40,7 @@ if echo "$FILE" | grep -q "$PATTERN";
  then
   echo "already a cron job!"
  else
-  line="*/30 * * * * $LOCATION/$CRONFILE"
+  line="*/30 * * * * . $HOME/.profile; $LOCATION/$CRONFILE"
   (crontab -l; echo "$line" ) | crontab -
 fi
 
