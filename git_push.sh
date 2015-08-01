c0d8003b461c5cd7498bd138f0ea1c41681453af
Ethan Hathaway <erhathaway@gmail.com>
2015-08-01 15:06:28 -0400

    test

--- git_push.sh
+++ git_push.sh
@@ -45,7 +45,7 @@ fi
 #if no errors, push to remote
 if [ "$PROCEED" == 1 ]; then
   # cd $DIRECTORY && git push
-  echo "$(date),SUCCESS,$ITEMTYPE,$DIRECTORY,$NAME,$COMMENT" >> $LOG
+  echo "$(date),SUCCESS,$ITEMTYPE,$DIRECTORY,$NAME,$COMMENT" #>> $LOG
 else
-  echo "$(date),ERROR,${ITEMTYPE},${DIRECTORY},${NAME},${COMMENT}" >> $LOG
+  echo "$(date),ERROR,${ITEMTYPE},${DIRECTORY},${NAME},${COMMENT}" # >> $LOG
 fi
