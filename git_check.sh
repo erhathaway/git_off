73a4494e6e0c6760a776cc3f2c337c556be0ca5e
Ethan Hathaway <erhathaway@gmail.com>
2015-08-02 00:15:47 -0400

    add check for internet connectivity

--- git_check.sh
+++ git_check.sh
@@ -12,16 +12,29 @@ GITPUSH=$BASEDIR/git_push.sh
 LOG=$BASEDIR/log.csv
 QUEUE=$BASEDIR/queue.csv
 
+# check for internet connectivity
+wget -q --spider http://github.com
 
+if [ $? -eq 0 ]; then
+    echo "Online"
+    INTERNET=1
+else
+    echo "Offline"
+    INTERNET=0
+fi
+
+#check to make sure there are items in the queue
 NUMOFLINES=$(cat $QUEUE | wc -l )
 
-if [ $NUMOFLINES -gt 0 ]
+if [[ $NUMOFLINES -gt 0 && $INTERNET -eq 1 ]]
   then
+  # get time
   TIME=$(date +"%m-%d-%Y-h")
 
   PATTERN=$TIME
   FILE=$(cat $LOG)
 
+  # check to make sure nothing was commited today
   if echo "$FILE" | grep -q "$PATTERN";
    then
     "already commited"
